78973fefbe9dbfc9fa25ee1c8c962586522b4278
Saminu Eedris <hey@saminu.me>
2022-05-27 07:42:13 +0100

    initial commit

---
+++ saminu-portfolio/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <nav>
+          <a href='./'>Saminu</a>
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
